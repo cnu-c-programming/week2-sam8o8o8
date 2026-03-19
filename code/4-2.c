d1e77a885483c2c092dec9cb9406266ee4a82b38
sam8o8o8 <sam87608467@gmail.com>
2026-03-19 23:08:59 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -2,7 +2,12 @@
 
 int main()
 {
+    char c = '*';
+    for (int i=1; i<10; i++) {
+        for (int j=1; j<10; j++) {
+            printf("%d %c %d\n", i, c, j);
+        }
+    }
 
     return 0;
 }
-

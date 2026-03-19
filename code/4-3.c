83604473cd79369a131ab11bf2aff7a13d625a4f
sam8o8o8 <sam87608467@gmail.com>
2026-03-19 23:27:56 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,26 @@
 
 int main()
 {
+    int n;
+    int isTrue = 1;
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        isTrue = 0;
+    } else {
+        for (int i=2; i<n; i++) {
+            if (n % i == 0) {
+                isTrue = 0;
+                break;
+            }
+        }
+    }
+
+    if (isTrue == 1) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
 
     return 0;
 }
-

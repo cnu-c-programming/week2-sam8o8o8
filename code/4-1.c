43fb7bf18d868ec835e2bb62899d270b883b515d
sam8o8o8 <sam87608467@gmail.com>
2026-03-19 23:03:16 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,26 @@
 
 int main()
 {
+    int x, y;
+    char operator;
+
+    scanf("%d %d %c", &x, &y, &operator);
+
+    if (operator == '+') {
+        printf("%d", x + y);
+    } else if (operator == '-') {
+        printf("%d", x - y);
+    } else if (operator == '*') {
+        printf("%d", x * y);
+    } else if (operator == '/') {
+        if (y == 0) {
+            printf("0으로 나눌 수 없음");
+        } else {
+            printf("%d", x / y);
+        }
+    } else {
+        printf("올바른 연산자를 입력하세요");
+    }
 
     return 0;
 }
-
